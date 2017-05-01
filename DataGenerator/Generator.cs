2bdef22dd58e670b40087899f596a279383ea9ba
Johan Lindvall <johan.lindvall@gmail.com>
2017-05-01 20:28:27 +0200

    Updated license to MIT. Removed redundant withoutType check.

--- DataGenerator/Generator.cs
+++ DataGenerator/Generator.cs
@@ -1,11 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
+﻿// Copyright(c) 2017 Johan Lindvall
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 namespace DataGenerator
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
     /// <summary>
     /// Defines a class for generating object hierarchies.
     /// </summary>
@@ -258,7 +278,7 @@ namespace DataGenerator
         /// <summary>
         /// Gets the object of the specified type.
         /// </summary>
-        /// <returns>The objectsx.</returns>
+        /// <returns>The objects.</returns>
         public IEnumerable<object> GetObjects()
         {
             return this.createdObjects;
@@ -558,11 +578,6 @@ namespace DataGenerator
                             }
                             else
                             {
-                                if (this.withoutType.Contains(elementType))
-                                {
-                                    continue;
-                                }
-
                                 if (singleton)
                                 {
                                     continue;
