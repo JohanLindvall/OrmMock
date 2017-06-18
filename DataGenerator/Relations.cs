17f166b6dbafa71c15902a75c25a8d84847ff41a
Johan Lindvall <johan.lindvall@gmail.com>
2017-06-18 15:41:46 +0200

    Adde support for foreign keys.

--- DataGenerator/Relations.cs
+++ DataGenerator/Relations.cs
@@ -37,11 +37,120 @@ namespace DataGenerator
         private readonly Dictionary<Type, PropertyInfo[]> primaryKeys = new Dictionary<Type, PropertyInfo[]>();
 
         /// <summary>
+        /// Holds the dictionary of type, type to foreign key properties
+        /// </summary>
+        private readonly Dictionary<Tuple<Type, Type>, PropertyInfo[]> foreignKeys = new Dictionary<Tuple<Type, Type>, PropertyInfo[]>();
+
+        /// <summary>
         /// Registers an expression defining the primary keys of an object.
         /// </summary>
         /// <typeparam name="T">The type of the object.</typeparam>
         /// <param name="expression">The expression defining the primary keys.</param>
         public void RegisterPrimaryKeys<T>(Expression<Func<T, object>> expression)
+            where T : class
+        {
+            this.primaryKeys.Add(typeof(T), this.GetPropertyInfo(expression));
+        }
+
+        /// <summary>
+        /// Registers an expression defining the foreign keys of an object.
+        /// </summary>
+        /// <typeparam name="TThis">The type of the object.</typeparam>
+        /// <typeparam name="TForeign">The type of the foreign object.</typeparam>
+        /// <param name="expression">The expression defining the foreign keys.</param>
+        public void RegisterForeignKeys<TThis, TForeign>(Expression<Func<TThis, object>> expression)
+            where TThis : class
+            where TForeign : class
+        {
+            this.foreignKeys.Add(new Tuple<Type, Type>(typeof(TThis), typeof(TForeign)), this.GetPropertyInfo(expression));
+        }
+
+        /// <summary>
+        /// Gets the primary key properties for a given type.
+        /// </summary>
+        /// <param name="t">The type of the object.</param>
+        /// <returns></returns>
+        public PropertyInfo[] GetPrimaryKeys(Type t)
+        {
+            PropertyInfo[] result;
+
+            if (!this.primaryKeys.TryGetValue(t, out result))
+            {
+                var key = t.GetProperty("Id");
+
+                if (key == null)
+                {
+                    throw new InvalidOperationException($@"Unable to determine key for type '{t.Name}'.");
+                }
+
+                this.primaryKeys.Add(t, new[] { key });
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets the primary key properties for a given type.
+        /// </summary>
+        /// <param name="tThis">The type of this object.</param>
+        /// <param name="tForeign">The type of the foreign object..</param>
+        /// <returns></returns>
+        public PropertyInfo[] GetForeignKeys(Type tThis, Type tForeign)
+        {
+            PropertyInfo[] result;
+
+            var key = new Tuple<Type, Type>(tThis, tForeign);
+            if (!this.foreignKeys.TryGetValue(key, out result))
+            {
+                var foreign = tThis.GetProperty(TrimTypeName(tForeign.Name) + "Id");
+
+                if (foreign == null)
+                {
+                    throw new InvalidOperationException($@"Unable to determine foreign key for type '{tThis.Name}' to '{tForeign.Name}'.");
+                }
+                result = new[] { foreign };
+                this.foreignKeys.Add(key, result);
+            }
+
+            var primary = this.GetPrimaryKeys(tForeign);
+
+            var mismatch = false;
+
+            if (result.Length != primary.Length)
+            {
+                mismatch = true;
+            }
+            else
+            {
+                for (var i = 0; i < result.Length; ++i)
+                {
+                    if (!object.ReferenceEquals(result[i], primary[i]))
+                    {
+                        mismatch = true;
+                        break;
+                    }
+                }
+            }
+
+            if (mismatch)
+            {
+                throw new InvalidOperationException($"Primary keys and foreign keys for '{tForeign}' in '{tThis}' do not match.");
+            }
+
+            return result;
+        }
+
+        private static string TrimTypeName(string input)
+        {
+            if (input.StartsWith("Db", StringComparison.Ordinal))
+            {
+                return input.Substring(2);
+            }
+
+            return input;
+        }
+
+        private PropertyInfo[] GetPropertyInfo<T>(Expression<Func<T, object>> expression)
             where T : class
         {
             var body = expression.Body;
@@ -59,46 +168,20 @@ namespace DataGenerator
 
                 if (mes.All(t => t != null))
                 {
-                    this.primaryKeys.Add(typeof(T), mes.Select(a => a.Member as PropertyInfo).ToArray());
-                    return;
+                    return mes.Select(a => a.Member as PropertyInfo).ToArray();
                 }
             }
             else if ((me = body as MemberExpression) != null)
             {
                 var pi = me.Member as PropertyInfo;
+
                 if (pi != null)
                 {
-                    this.primaryKeys.Add(typeof(T), new[] { me.Member as PropertyInfo });
+                    return new[] { pi };
                 }
-                return;
             }
 
             throw new InvalidOperationException("Unsupported expression.");
-        }
-
-        /// <summary>
-        /// Gets the primary key properties for a given type.
-        /// </summary>
-        /// <param name="t"></param>
-        /// <returns></returns>
-        public PropertyInfo[] GetPrimaryKeys(Type t)
-        {
-            PropertyInfo[] result;
-
-            if (!this.primaryKeys.TryGetValue(t, out result))
-            {
-                var key = t.GetProperty("Id");
-
-                if (key == null)
-                {
-                    throw new InvalidOperationException($@"Unable to determine key for type '{t.Name}'.");
-                }
-
-                result = new[] { key };
-                this.primaryKeys.Add(t, result);
-            }
-
-            return result;
         }
     }
 }
