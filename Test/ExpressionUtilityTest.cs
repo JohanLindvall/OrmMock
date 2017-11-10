bd7b12f499cac3ee120b0545140ba689950fec7a
Johan Lindvall <johan.lindvall@gmail.com>
2017-11-10 16:01:49 +0100

    Added more tests.

---
+++ Test/ExpressionUtilityTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using DataGenerator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class ExpressionUtilityTest
+    {
+        public class TestClass
+        {
+            public int Id { get; set; }
+
+            public string Str { get; set; }
+        }
+
+        [TestMethod]
+        public void TestMemberExpression()
+        {
+            var ids = ExpressionUtility.GetPropertyInfo<TestClass, int>(tc => tc.Id);
+            Assert.AreSame(typeof(TestClass).GetProperty(nameof(TestClass.Id)), ids.Single());
+        }
+
+        [TestMethod]
+        public void TestNewExpression()
+        {
+            var ids = ExpressionUtility.GetPropertyInfo<TestClass, object>(tc => new { tc.Id, tc.Str });
+            Assert.AreSame(typeof(TestClass).GetProperty(nameof(TestClass.Id)), ids[0]);
+            Assert.AreSame(typeof(TestClass).GetProperty(nameof(TestClass.Str)), ids[1]);
+            Assert.AreEqual(2, ids.Length);
+        }
+
+        [TestMethod]
+        public void TestUnaryExpression()
+        {
+            var ids = ExpressionUtility.GetPropertyInfo<TestClass, object>(tc => (object)tc.Id);
+            Assert.AreSame(typeof(TestClass).GetProperty(nameof(TestClass.Id)), ids.Single());
+        }
+
+        [TestMethod]
+        public void TestUnsupported()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() => ExpressionUtility.GetPropertyInfo<TestClass, object>(tc => "foo"));
+        }
+    }
+}
